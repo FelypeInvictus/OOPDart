aac1601f0d668a5ac746400db81f5c1379752885
FelypeInvictus <felype.sales@outlook.com>
2023-02-18 10:25:47 -0300

    Inserção de dados

--- main.dart
+++ main.dart
@@ -1,5 +1,27 @@
-import 'pessoa.dart';
+import 'lib/cartao.dart';
+import 'lib/cliente.dart';
+import 'lib/compra.dart';
+import 'lib/conta.dart';
+import 'lib/fatura.dart';
+
+/*
+* Author: Felype Rangel <nickname: Felype Invictus> <felype.invictus@gmail.com>
+*/
 
 void main() {
-  Pessoa(nome: 'Felype', sobrenome: 'Rangel');
+//Dados ficticios
+
+  var conta = Conta(
+    cliente: Cliente(id: '033', cpf: '120.345.245-97', nome: 'Felype', sobrenome: ''),
+    cartao: Cartao(limite: 1230, numero: '1234556778', mes: 03, ano: 2026, codigo: 123),
+    faturas: [
+      Fatura(
+        compras: [
+          Compra(preco: 33, descricao: 'Fone de ouvido', data: '21/04'),
+        ],
+        mes: 7,
+        ano: 2021,
+      )
+    ],
+  );
 }
